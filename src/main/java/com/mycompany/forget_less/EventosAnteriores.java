65d5ec20ea1f7fbd3f3a273f53fc63ea8ebea409
Joao Vitor <joaovitor.h.bastos.07@gmail.com>
2021-03-23 16:46:13 -0400

    testei

--- src/main/java/com/mycompany/forget_less/EventosAnteriores.java
+++ src/main/java/com/mycompany/forget_less/EventosAnteriores.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//anacatota
 package com.mycompany.forget_less;
 
 /**
---
+++ src/main/java/com/mycompany/forget_less/bean/Eventos.java
@@ -0,0 +1,9 @@
+package com.mycompany.forget_less.bean;
+
+public class Eventos {
+
+    private int id;
+    private String nomeEvento;
+    private String descricaoEvento;
+    private String data;
+}
