0a700cdc0f44e3aa5797fbe304d357e6955caaea
Joao Vitor <joaovitor.h.bastos.07@gmail.com>
2021-03-23 17:12:33 -0400

    bean com classe notas

--- src/main/java/com/mycompany/forget_less/bean/Eventos.java
+++ src/main/java/com/mycompany/forget_less/bean/Eventos.java
@@ -1,9 +1,51 @@
 package com.mycompany.forget_less.bean;
 
-public class Eventos {
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+ 
+@Entity
+public class Eventos implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    
     private String nomeEvento;
     private String descricaoEvento;
     private String data;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomeEvento() {
+        return nomeEvento;
+    }
+
+    public void setNomeEvento(String nomeEvento) {
+        this.nomeEvento = nomeEvento;
+    }
+
+    public String getDescricaoEvento() {
+        return descricaoEvento;
+    }
+
+    public void setDescricaoEvento(String descricaoEvento) {
+        this.descricaoEvento = descricaoEvento;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
 }
